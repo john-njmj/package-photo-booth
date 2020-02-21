c593d51e5b3fd4def3103059af01f980386c53da
john-njmj <46443771+john-njmj@users.noreply.github.com>
2020-02-22 00:15:04 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -72,8 +72,7 @@ end
 local function find_renner(nr)
     local name 
     if racer_list ~= {} then 
-        name = "Lijst"
-        -- name = racer_list[2][3]
+        name = racer_list[2]
         --    name = "Niet gevonden - " .. nr
     else
         name = "Geen Lijst"
@@ -166,8 +165,8 @@ function node.render()
            previous_pic[next_pic(next_pic(last_pic))]:draw(85, 715, 275, 1000)
     end
     -- draw preview mask 
-    black:draw(980, 0, 1190, HEIGHT, 0.8)
-    black:draw(1600, 0, 1900, HEIGHT, 0.8)
+    black:draw(980, 0, 1190, HEIGHT, 1)
+    black:draw(1550, 0, 1920, HEIGHT, 0.8)
     -- old info for debuging 
     -- text_renner(HEIGHT-50, ren_nr, 50, 1,1,1,1)
     -- text_renner(HEIGHT-200, ren_nr_state, 50, 1,1,1,1)
